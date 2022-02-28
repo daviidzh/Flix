e48f4ee3f35b60031069f82872bd352bbeb2efe0
David Zhao-Dian <96024949+daviidzh@users.noreply.github.com>
2022-02-27 23:45:04 -0500

    added functionality for viewing movie posters and added alamofire pod

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -53,11 +54,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "https://image.tdmb.org/t/p/w185"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)
+        let posterUrl = URL(string: baseUrl + posterPath)!
        
-        
+        cell.posterView.af.setImage(withURL: posterUrl)
         
         return cell
     }
